e8c1484fb0664682237f985999385025a156575c
Juan Esteban LC <juanelc197@outlook.com>
2016-09-18 13:58:01 -0500

    (API) Agregando en la carpeta de models las clases de User y EquipmentType.

---
+++ IneqApi/IneqApi/Models/EquipmentType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IneqApi.Models
+{
+    public class EquipmentType
+    {
+        public int ID { get; set; }
+        public string Description { get; set; }
+        public float UsefulLife { get; set; }
+        public float GuaranteeDuration { get; set; }
+        public bool Active { get; set; }
+
+
+
+    }
+}
