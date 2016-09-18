82fc6d1caa94933ffea0b2bee42577607fd20adc
armandoomtzz <armandoomtzz@gmail.com>
2016-09-18 18:05:40 -0500

    API: Verificacion de funcionamiento de controllers

---
+++ IneqApi/IneqApi/Models/MigrationHistory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IneqApi.Models
+{
+    public class MigrationHistory
+    {
+        public string MigrationId { get; set; }
+        public string ContextKey { get; set; }
+        public int Model { get; set; }
+        public string ProductVersion { get; set; }
+
+    }
+}
