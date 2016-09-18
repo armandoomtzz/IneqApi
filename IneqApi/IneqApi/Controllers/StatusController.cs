0df42322f36e0edf5e7c0247f0709d0994f89e43
ErickaHernandez <ericka_bfc@hotmail.com>
2016-09-17 23:07:38 -0500

    Api se realizo StatusController en CONTROLLERS

---
+++ IneqApi/IneqApi/Controllers/StatusController.cs
@@ -0,0 +1,77 @@
+﻿using IneqApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace IneqApi.Controllers
+{
+    public class StatusController : ApiController
+    {
+        IneqApiContext dc = new IneqApiContext();
+
+        // READ ALL
+        public ActionResult List()
+        {
+            return View(dc.Status.ToList());
+        }
+        //READ SINGLE
+        public ActionResult Details(int id = 0)
+        {
+            return View(dc.Status.Find(id));
+        }
+        //CREATE
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost, ValidateAntiForgeryToken]
+        public ActionResult Create(Status br)
+        {
+            using (dc)
+            {
+                dc.Status.Add(br);
+                dc.SaveChanges();
+
+            }
+            return RedirectToAction("List");
+        }
+        //UPDATE
+        public ActionResult Edit(int id = 0)
+        {
+            return View(dc.Status.Find(id));
+        }
+        [HttpPost, ValidateAntiForgeryToken]
+        public ActionResult Edit(Status br)
+        {
+            dc.Entry(br).State = EntityState.Modified;
+            dc.SaveChanges();
+            return RedirectToAction("List");
+        }
+
+        //DELETE
+        public ActionResult Delete(int id = 0)
+        {
+            return View(dc.ComponentTypes.Find(id));
+        }
+
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult delete_conf(int id)
+        {
+            Status br = dc.Statuses.Find(id);
+            dc.Status.Remove(eq);
+            dc.SaveChanges();
+            return RedirectToAction("List");
+        }
+
+        private ActionResult RedirectToAction(string v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
